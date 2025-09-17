09dd8e0464f6efa1c2a8464f5505d802204f736c
Onour Imprachim <onuribrahim00@gmail.com>
2025-09-17 14:10:26 +0300

    Update dbConnection.php

--- PHPDbServices/dbConnection.php
+++ PHPDbServices/dbConnection.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$database = "database_name";
+$database = "physiotherapy";
 // Establishing connection to the database
 $connection = mysqli_connect($host, $user, $password, $database) or die("Connection failed: " . mysqli_connect_error());
 
